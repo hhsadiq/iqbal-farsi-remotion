9965b0265fbcc3e296680714ea4f507e8d3aa5e3
Hassaan Sadiq <hhsadiq@gmail.com>
2023-12-13 10:48:07 +0500

    added the nastaliq urdu fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
 	content: ['./src/**/*.{ts,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+        'nastaliq': ['Noto Nastaliq Urdu', 'sans-serif'],
+        'nastaliq-medium': ['Noto Nastaliq Urdu Medium', 'sans-serif'],
+        'nastaliq-semibold': ['Noto Nastaliq Urdu SemiBold', 'sans-serif'],
+        'nastaliq-bold': ['Noto Nastaliq Urdu Bold', 'sans-serif'],
+        'nastaliq-variable': ['Noto Nastaliq Urdu Variable', 'sans-serif'],
+      },
+		},
 	},
 	plugins: [],
 };
