5cbcc01c9a7bcbdd8971ef7c34f71221d5148609
Hassaan Sadiq <hhsadiq@gmail.com>
2023-12-24 22:29:07 +0500

    added the slide transition

--- src/components/Composition.tsx
+++ src/components/Composition.tsx
@@ -3,6 +3,16 @@ import React from 'react';
 import { z } from 'zod';
 import { Couplet } from './Couplet';
 import { Audiograms } from './Audiograms';
+import {
+	linearTiming,
+	springTiming,
+	TransitionSeries,
+} from "@remotion/transitions";
+import { SlideDirection } from "@remotion/transitions/slide";
+import { slide } from "@remotion/transitions/slide";
+
+
+const slideDirection: SlideDirection = "from-right";
 
 export const coupletSchema = z.object({
 	number: z.number(),
@@ -34,36 +44,40 @@ export const MyComposition: React.FC<z.infer<typeof myCompSchema>> = ({
 }) => {
 	const fps = 30;
 	const audioPath = poemPath + 'audio.m4a';
-	const { durationInFrames } = useVideoConfig();
+	const { durationInFrames, width, height } = useVideoConfig();
 	let time: number = 0;
 
 	if (data.couplets && data.couplets.length > 0 && data.couplets[0].startTime !== undefined) {
 		time = data.couplets[0].startTime;
 	}
 
-
 	let firstCoupletStartTime = Math.ceil((time) * fps);
 
 	return (
 		<AbsoluteFill className="bg-gray-100 flex flex-col items-center justify-center">
 			<Audio src={staticFile(audioPath)} placeholder='persian-recitation' />
-			<Series>
+			<TransitionSeries>
 				{data.couplets.map((couplet, i) => {
 					const durationInFrames = Math.ceil((couplet.endTime - couplet.startTime) * fps);
 					firstCoupletStartTime = i == 0 ? firstCoupletStartTime : 0;
 
 					return (
-						<Series.Sequence
-							key={couplet.number}
-							offset={firstCoupletStartTime}
-							durationInFrames={durationInFrames}
-							layout="none"
-						>
-							<Couplet couplet={couplet} data={data} />
-						</Series.Sequence>
+						<React.Fragment key={couplet.number}>
+							<TransitionSeries.Sequence
+								offset={firstCoupletStartTime}
+								durationInFrames={durationInFrames}
+								layout="none"
+							>
+								<Couplet couplet={couplet} data={data} />
+							</TransitionSeries.Sequence>
+							<TransitionSeries.Transition
+								timing={springTiming({ config: { damping: 33 } })}
+								presentation={slide({direction: slideDirection})}
+							/>
+						</React.Fragment>
 					);
 				})}
-			</Series>
+			</TransitionSeries>
 
 
 			{/* Dynamic Audio Visualization */}
