9adbd520a80e55fcb824bd6e0d33124f8ac73342
Hassaan Sadiq <hhsadiq@gmail.com>
2024-07-03 22:43:31 +0500

    fixed the channel links outro

--- src/components/ChannelIntro.tsx
+++ src/components/ChannelIntro.tsx
@@ -33,7 +33,7 @@ export const ChannelInro: React.FC<{}> = () => {
         </p>
       </div>
       {/* Row 5 */}
-      <div className="flex items-top justify-center w-full h-[50%] px-36 mt-[-256px]">
+      <div className="flex items-top justify-center w-full h-[20%] px-36 mt-[116px]">
         <img alt="Rectangle" src={staticFile('img/channel-icons-and-names.svg')} />
       </div>
     </div>
